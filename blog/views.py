d94ef0dedcd445dfaf4c52d2ad1ea51acc511f16
Isaac Hemans <isaac.hemans@metro.digital>
2023-05-13 10:26:46 +0000

    Finish customer template tags

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from blog.models import Post
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def index(request):
   posts = Post.objects.all()
   print(posts)
   return render(request, "blog/index.html", {"posts": posts})
+
+def post_detail(request, slug):
+  post = get_object_or_404(Post, slug=slug)
+  return render(request, "blog/post-detail.html", {"post": post})
