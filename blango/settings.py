cdbc24918fb515675b9bd9e2ce1918acdf4dd85b
Isaac Hemans <isaac.hemans@metro.digital>
2023-05-14 19:29:38 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -25,7 +25,7 @@ class Dev(Configuration):
     DEBUG = values.BooleanValue(True)
     ALLOWED_HOSTS = ['*']
 
-    X_FRAME_OPTIONS = 'ALLOW-FROM ' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'
+    #X_FRAME_OPTIONS = 'ALLOW-FROM ' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'
     CSRF_COOKIE_SAMESITE = None
     CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io']
     CSRF_COOKIE_SECURE = True
@@ -38,6 +38,11 @@ class Dev(Configuration):
     AUTH_USER_MODEL = "blango_auth.User"
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     ACCOUNT_ACTIVATION_DAYS = 7
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
     # Application definition
 
@@ -48,10 +53,12 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         'crispy_forms',
         'crispy_bootstrap5',
         'blog',
+        "allauth", "allauth.account", "allauth.socialaccount", "allauth.socialaccount.providers.google",
     ]
 
     MIDDLEWARE = [
