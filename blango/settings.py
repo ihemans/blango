8cd0988960d8297eba6d336f170f68fc5bef40ca
Isaac Hemans <isaac.hemans@metro.digital>
2023-06-03 20:18:43 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -55,6 +55,7 @@ class Dev(Configuration):
         'django.contrib.messages',
         'django.contrib.sites',
         'django.contrib.staticfiles',
+        'rest_framework',
         'crispy_forms',
         'crispy_bootstrap5',
         'blog',
