f8bb27405a9e9df1465b113e2e924dfee146e340
Isaac Hemans <isaac.hemans@metro.digital>
2023-06-04 19:00:34 +0000

    Finish caching

--- blog/api/views.py
+++ blog/api/views.py
@@ -4,6 +4,11 @@ from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
 from blango_auth.models import User
 from blog.models import Post
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers, vary_on_cookie
+
+from rest_framework.exceptions import PermissionDenied
 
 
 class PostList(generics.ListCreateAPIView):
